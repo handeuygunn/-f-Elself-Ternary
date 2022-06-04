6a195f42c423c0d110a329a78fc29569943b3203
Hande Nur UYGUN <104138485+handeuygunn@users.noreply.github.com>
2022-06-05 00:47:19 +0300

    Add files via upload

---
+++ it-elself/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+     static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if(time >= 6 && time<= 12)
+        Console.WriteLine("günaydın");
+        else if(time >= 12 && time <= 18)
+        Console.WriteLine("iyi günler");
+        else
+        Console.WriteLine("iyi geceler");
+
+        string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+        
+        sonuc = time >= 6  && time<11 ? "günaydın" : time<=18 ? "iyi günler": "iyi geceler";     
+           Console.WriteLine(sonuc);
+
+    }
+}
